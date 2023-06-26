7351181b1194528c370f8da1f512bea8557ddcef
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-06-26 18:50:50 -0300

    IP.py

--- IP.py
+++ IP.py
@@ -2,74 +2,99 @@ import socket
 import whois
 import time
 import geocoder
+import platform
 import os
+import requests
+import colorama
+from colorama import Fore, Style
 #import nmap
 #from ipwhois import IPWhois
 
 #Socket site: https://docs.python.org/3/library/socket.html
 
 #The script is in development very likely not to be 100% sure in searches.
-#I used Nmap to be more sure, in the searches of the open doors.
-
-print(" ▄█▀▀▀█▄█ ▄▄█▀▀██▄   ▄▄█▀▀▀█▄█████▀ ▀███▀▀███▀▀▀██████▀▀██▀▀███")
-print("▄██    ▀███▀    ▀██▄██▀     ▀█ ██   ▄█▀    ██    ▀██▀   ██   ▀█")
-print("▀███▄   ██▀      ▀███▀       ▀ ██ ▄█▀      ██   █       ██     ")
-print("  ▀█████▄█        ███          █████▄      ██████       ██     ")
-print("▄     ▀███▄      ▄███▄         ██  ███     ██   █  ▄    ██     ")
-print("██     ████▄    ▄██▀██▄     ▄▀ ██   ▀██▄   ██     ▄█    ██     ")
-print("█▀█████▀  ▀▀████▀▀   ▀▀█████▀▄████▄   ███▄██████████  ▄████▄   ")
-print('\n')
-print('#############')
-print('BY: ~#M?x')
-print('GitHub:','https://github.com/suchsoak')
-print('#############.\n')
-
-print("Make sure you are an Administrator!!!\n")
-
-print('Exemple: URL www.google.com or IP 127.154.25.6\t\n',)
-
-Host = input(f'put the addres Host (IP ou URL): ')  
+colorama.init()
+print(Fore.RED)
+print("  ██████     |")
+print("▒██    ▒     | BY: ~#M?x ")
+print("░ ▓██▄       | GitHub: https://github.com/suchsoak")
+print("  ▒   ██▒    | ")
+print("▒██████▒▒    |")
+print("▒ ▒▓▒ ▒ ░    |")
+print("░ ░▒  ░      |")
+print("░  ░  ░      |")
+print("      ░      |")
+print("\n")
+Host = input(f'IP ou URL: ') 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(Style.RESET_ALL)
 
-class ping_ns_():
-    time.sleep(5.0)
-    print("\n")
-    print("*****************************\n")
-    print("---------------------------\n")
-    print('[!] Ping -n 4', Host)
-    time.sleep(5.0)
-    print('---------------------------\n')
-    command = f"ping -n 4 {Host}"
-    os.system(command)
-    print("\n")
-    print("*****************************\n")
-    print('---------------------------\n')
-    print('[!] Nslookup -type=mx', Host)
-    print('---------------------------')
-    time.sleep(5.0)
-    command_2 = f"nslookup -type=mx {Host}"
-    os.system(command_2)
-    print("\n")
-    print("*****************************\n")
-    print('---------------------------\n')
-    print('[!] Tracert -h 8', Host)
-    print('---------------------------\n')
-    time.sleep(5.0)
-    command_2 = f"tracert -h 8 {Host}"
-    os.system(command_2)
-    print("\n")
-    print("*************************\n")
-    print('---------------------------\n')
-    print('[!] pathping -h 4', Host)
-    print('---------------------------\n')
-    time.sleep(5.0)
-    command_2 = f"pathping -p 5.0 -h 4 {Host}"
-    os.system(command_2)
-    print("\n")
 
-g = geocoder.ip (Host)
-go = geocoder.google (Host)
+# r = requests.get()
+
+
+
+if platform.system() == 'Windows': 
+        # time.sleep(5.0)
+         print("\n")
+         print("---------------------------\n")
+         print('[!] Ping -n 4', Host)
+         time.sleep(5.0)
+         print('---------------------------\n')
+         command = f"ping -n 4 {Host}"
+         os.system(command)
+
+        # print('---------------------------\n')
+        # print('[!] Nslookup -type=mx', Host)
+        # print('---------------------------')
+        # time.sleep(5.0)
+        # command_2 = f"nslookup -type=mx {Host}"
+        # os.system(command_2)
+        # print("\n")
+        # print("*****************************\n")
+        # print('---------------------------\n')
+        # print('[!] Tracert -h 8', Host)
+        # print('---------------------------\n')
+        # time.sleep(5.0)
+        # command_3 = f"tracert -h 8 {Host}"
+        # os.system(command_3)
+        # print("\n")
+        # print("*************************\n")
+        # print('---------------------------\n')
+        # print('[!] pathping -h 4', Host)
+        # print('---------------------------\n')
+        # time.sleep(5.0)
+        # command_4 = f"pathping -p 5.0 -h 4 {Host}"
+        # os.system(command_4)
+        # print("\n")
+
+elif platform.system() == 'Linux': 
+        time.sleep(5.0)
+        print("\n")
+        print("*****************************\n")
+        print("---------------------------\n")
+        print('[!] Ping -c 4', Host)
+        time.sleep(5.0)
+        print('---------------------------\n')
+        command = f"ping -c 4 {Host}"
+        os.system(command)
+elif platform.system() == 'Darwin':
+        time.sleep(5.0)
+        print("\n")
+        print("*****************************\n")
+        print("---------------------------\n")
+        print('[!] Ping -c 4', Host)
+        time.sleep(5.0)
+        print('---------------------------\n')
+        command = f"ping -c 4 {Host}"
+        os.system(command)
+
+else :
+    print("operating system not supported.")
+    
+g = geocoder.ip (f'{Host}')
+go = geocoder.google (f'{Host}')
 
 class quem_e():
     try:
@@ -79,44 +104,63 @@ class quem_e():
         whois_resultado = whois.whois(Host)
         ip = socket.gethostbyname(Host)
         sem_informacao = False
-
-        print('--------------------------')
-        print("[!] IP:", ip)
-        print('--------------------------')
-        print("[!] Postcode street:", go.postal)
-        print('--------------------------')
-        print("[!] Street:", go.street)
-        print('--------------------------')
-        print("[!] More information:", go.street_long)
-        print('--------------------------')
-        print('[!] house number:', go.housenumber)
-        print('--------------------------')
-        print('[!] city:', g.city)
-        print('--------------------------')
-        print("[!] Admin contact: ", whois_resultado.admin_contact)
-        print('--------------------------')
-        print("[!] tech contact: ", whois_resultado.tech_contact)
-        print('--------------------------')
-        print('[!] country: ', g.country)
-        print('--------------------------')
-        print("[!] lat: ", g.lat)
-        print('--------------------------')
-        print("[!] lng: ", g.lng)
-        print('--------------------------')
-        print("[!] Domain name:", whois_resultado.domain_name)
-        print('--------------------------')
-        print("[!] Creation date:", whois_resultado.creation_date)
-        print('--------------------------')
-        print("[!] Experation date:", whois_resultado.expiration_date)
-        print('--------------------------')
-        print("[!] Updated date:", whois_resultado.updated_date)
-        print('--------------------------')
-        print("[!] Status:", whois_resultado.status)
-        print('--------------------------')
-        print("[!] Name servers:", whois_resultado.name_servers)
-        print('--------------------------')
-        print("[!] Registrant:", whois_resultado.registrant)
-        print('---------------------------------------------------------')
+        if ip is not None:
+            colorama.init()
+            print(Fore.RED)
+            print('----------------------------------')
+            print("[*] IP:", ip)
+            print('----------------------------------')  
+            print(Style.RESET_ALL)  
+        if go.posta is not None:
+            print("[*] Postcode street:", go.postal)
+            print('----------------------------------')
+        if go.street is not None:    
+            print("[*] Street:", go.street)
+            print('----------------------------------')
+        if go.street_long is not None:
+            print("[*] More information:", go.street_long)
+            print('----------------------------------')
+        if go.housenumber is not None:
+            print('[*] house number:', go.housenumber)
+            print('----------------------------------')
+        if g.city is not None:
+            print('[*] city:', g.city)
+            print('----------------------------------')
+        if whois_resultado.admin_contact is not None:
+            print("[*] Admin contact: ", whois_resultado.admin_contact)
+            print('----------------------------------')
+        if whois_resultado.tech_contact is not None:
+            print("[*] tech contact: ", whois_resultado.tech_contact)
+            print('----------------------------------')
+        if g.country is not None:
+            print('[*] country: ', g.country)
+            print('----------------------------------')
+        if g.lat is not None:
+            print("[*] lat: ", g.lat)
+            print('----------------------------------')
+        if g.lng is not None:
+            print("[*] lng: ", g.lng)
+            print('----------------------------------')
+        if whois_resultado.domain_name is not None:
+            print("[*] Domain name:", whois_resultado.domain_name)
+            print('----------------------------------')
+        if whois_resultado.creation_date is not None:
+            print("[*] Creation date:", whois_resultado.creation_date)
+            print('----------------------------------')
+        if whois_resultado.expiration_date is not None:
+            print("[*] Experation date:", whois_resultado.expiration_date)
+            print('----------------------------------')
+        if whois_resultado.updated_date is not None:
+            print("[*] Updated date:", whois_resultado.updated_date)
+            print('----------------------------------')
+        if whois_resultado.status is not None:
+            print("[*] Status:", whois_resultado.status)
+            print('----------------------------------')
+        if whois_resultado.name_servers is not None:
+            print("[*] Name servers:", whois_resultado.name_servers)
+            print('----------------------------------')
+        if whois_resultado.registrant is not None:
+            print("[*] Registrant:", whois_resultado.registrant)
     except KeyboardInterrupt:
         print('program stopped')
     except Exception as erro:
@@ -128,41 +172,48 @@ print('------------------\n')
 service = socket.getservbyport
 
 s.settimeout(15.0) 
-nenhuma = True
+no = True
  
-ports = [21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 993, 995]
-class Portas:    
-    time.sleep(6.0) 
-    for port in ports:   
-        resultado = s.connect_ex((Host, port))
-        if resultado == 0:
-            print('**************')
-            print('[*] Scanning: ', Host)
-            print('[*] Open: ', (port), (service(port)))
-            print('**************')
-            print('\t\n')  
-            nenhuma = False 
-    else:
-        pass
-        if nenhuma:
-            print("No ports found by scanner socket.")
-
-class nmap():
+#ports = [21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 993, 995]
+ports = range(1, 65535)
+class Portas: 
     try:
-        print('-----------------\t\n')
-        print('For a scnner, more accurate...')
-        print('-----------------')
-        print('[!] Nmap -v -sV', Host)
-        time.sleep(5.0)
-        nmapscanner = f'nmap -v -sV {Host}'
-        os.system(nmapscanner)  
-        print('------------------\t\n')
-        print('[!] Nmap -Pn', Host)
-        print('\t\n')
-        print('------------------')
-        nmapscanner2 = f'nmap -Pn {Host}'
-        os.system(nmapscanner2)
+        time.sleep(6.0) 
+        for port in ports:   
+            resultado = s.connect_ex((Host, port))
+            if resultado == 0:
+                    print('||')
+                    print('[*] Open: ', (port), (service(port)))
+                    print('||')
+                    print('\t\n') 
+                    no = False
+        else:
+                pass
+                if no:
+                    print("No ports found.")
     except KeyboardInterrupt:
-        print('program stopped')
+         print('program stopped')
     except Exception as erro:
-        print('An error has occurred', erro)
+         print(erro)
+                    
+
+# class nmap():
+#     try:
+#         print('-----------------\t\n')
+#         print('For a scnner, more accurate...')
+#         print('-----------------')
+#         print('[!] Nmap -v -sV', Host)
+#         time.sleep(5.0)
+#         nmapscanner = f'nmap -v -sV {Host}'
+#         os.system(nmapscanner)  
+#         print('------------------\t\n')
+#         print('[!] Nmap -Pn', Host)
+#         print('\t\n')
+#         print('------------------')
+#         nmapscanner2 = f'nmap -Pn {Host}'
+#         os.system(nmapscanner2)
+#     except KeyboardInterrupt:
+#         print('program stopped')
+#     except Exception as erro:
+#         print(erro)
+
